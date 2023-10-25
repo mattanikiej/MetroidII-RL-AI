de7683cd1bd0c49e571aa9b067002d4b597caf7c
Matt Anikiej <mattanikiej@gmail.com>
2023-10-25 00:31:42 -0500

    ainitial commit - env bot makes moves it's given

---
+++ metroid_env.py
@@ -0,0 +1,158 @@
+# std libs
+import random
+
+# data/ math libs
+import numpy as np
+
+# machine learning libs
+from gymnasium import Env, spaces
+
+# pyboy 
+from pyboy import PyBoy
+from pyboy.utils import WindowEvent
+
+class MetroidGymEnv(Env):
+    def __init__(self, config=None):
+        """
+        Constructor for MetroidGymEnv
+        
+        :param config (dict): configuration settings for the environment
+        """
+        # check a config was passed in
+        if config is None:
+            raise Exception("Config needs to be set for MetroidGymEnv")
+        
+        # load in config values
+        self.action_frequency = config['action_frequency']
+        self.initial_state = config['initial_state']
+        self.rom_path = config['rom_path']
+        self.seed = config['seed']
+
+        # initialize movement
+        self.valid_actions = [
+            # move samus
+            WindowEvent.PRESS_ARROW_DOWN,
+            WindowEvent.PRESS_ARROW_LEFT,
+            WindowEvent.PRESS_ARROW_UP,
+            WindowEvent.PRESS_ARROW_RIGHT,
+
+            WindowEvent.RELEASE_ARROW_DOWN,
+            WindowEvent.RELEASE_ARROW_LEFT,
+            WindowEvent.RELEASE_ARROW_UP,
+            WindowEvent.RELEASE_ARROW_RIGHT,
+
+            # jump/ shoot
+            WindowEvent.PRESS_BUTTON_A,
+            WindowEvent.PRESS_BUTTON_B,
+
+            WindowEvent.RELEASE_BUTTON_A,
+            WindowEvent.RELEASE_BUTTON_B,
+
+            # toggle missles
+            WindowEvent.PRESS_BUTTON_SELECT
+        ]
+
+        self.last_pressed = None
+
+        # load in the emulator and game
+        self.pyboy = PyBoy(self.rom_path)
+
+        self.screen = self.pyboy.botsupport_manager().screen()
+
+        # set gym attributes
+        self.action_space = spaces.Discrete(len(self.valid_actions))
+        # self.metadata = {"render.modes": []}
+        self.reward_range = (0, 15000)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(144, 160, 3), dtype=np.uint8)
+            
+        # start the game from initial state
+        self.reset()
+
+
+    def step(self, action):
+        """
+        Updates an environment with actions returning the next agent observation, 
+        the reward for taking that actions, if the environment has terminated or 
+        truncated due to the latest action and information from the environment 
+        about the step, i.e. metrics, debug info.
+
+        https://gymnasium.farama.org/api/env/
+
+        :param action (actType): action the model selected to run
+
+        :return: (ObsType), (SupportsFloat), (bool), (bool), (dict)
+        """
+        self.act(action)
+
+        return self.observation_space, 0, False, False, {}
+
+
+    def reset(self, seed=None):
+        """
+        Resets the environment to an initial state, required before calling step. 
+        Returns the first agent observation for an episode and information, 
+        i.e. metrics, debug info.
+
+        https://gymnasium.farama.org/api/env/
+
+        :param seed (int): random state to use for rng
+
+        :return: (ObsType), (dict)
+        """
+        self.seed = seed
+
+        with open(self.initial_state, "rb") as f:
+            self.pyboy.load_state(f)
+
+        return self.render(), {}
+
+
+    def render(self):
+        """
+        Renders the environments to help visualise what the agent see, 
+        examples modes are “human”, “rgb_array”, “ansi” for text.
+
+        https://gymnasium.farama.org/api/env/
+
+        :return: (list[int])
+        """
+        # get screen pixels values
+        game_pixels = self.screen.screen_ndarray() # (144, 160, 3)
+        return game_pixels
+
+
+    def close(self):
+        """
+        Closes the environment, important when external software is used, 
+        i.e. pygame for rendering, databases
+
+        https://gymnasium.farama.org/api/env/
+        """
+        self.pyboy.close()
+
+
+
+    def act(self, action):
+        """
+        Sends the given action to the emulator
+
+        :param action (actType): action to send to pyboy
+        """
+        # only WindowEvent.PRESS_BUTTON_SELECT should be immediately released
+        select_pressed = False
+        if self.valid_actions[action] == WindowEvent.PRESS_BUTTON_SELECT:
+            select_pressed = True
+
+        # send action then tick self.action_frequency number of steps
+        self.pyboy.send_input(self.valid_actions[action])
+
+        for _ in range(self.action_frequency):
+            # advance game 1 frame
+            self.pyboy.tick()
+
+            # release select if pressed
+            if select_pressed:
+                select_pressed = False
+                self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_SELECT)
+
+        
